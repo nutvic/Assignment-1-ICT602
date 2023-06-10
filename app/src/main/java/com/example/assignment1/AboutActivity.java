c553f75b6f508a60ab8792eb6716388b993e3378
Nurul Nadia Nabilah binti Mohd Mazlan <nadianabilah27@gmail.com>
2023-06-10 16:46:33 +0800

    Final Commit

--- app/src/main/java/com/example/assignment1/AboutActivity.java
+++ app/src/main/java/com/example/assignment1/AboutActivity.java
@@ -2,11 +2,20 @@ package com.example.assignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
 
 public class AboutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    public void openGitHubPage(View view) {
+        String githubUrl = "https://github.com/nutvic/Assignment-1-ICT602";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(githubUrl));
+        startActivity(intent);
     }
 }
